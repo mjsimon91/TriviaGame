681715770e860daeb58c9b62f2f8d850f673a4ee
mjsimon91 <mjsimon91@gmail.com>
2017-12-21 19:44:56 -0500

    Fixed merge conflicts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,60 +1,143 @@
-var answer;
-var response;
-var questionTitle;
-var questionAnswer;
-var currentQuestion = 0;
-var correctAnswer = 0;
-var timer = $("#timer");
-var actualQuestion = $('#question');
-var answerChoices = $('#answerChoice');
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var counterNumber;
+var counter;
+var timer;
+var questionNumber = 0;
+var answerChoices = 0;
+var downloadTimer;
+//Creating all of my my questions
 
-//Creating an object that will hold all of the trivia questions
-
-var myQuestions = [{
-  questionId: 1,
-  question: "How many planets are in the solar system?",
-  answers: ["9","8","10","11"],
-  correctAnswer: myQuestions.answers[1]
-},
-{
-  questionId: 2,
-  question: "What is the diameter of the Sun?",
-  answers: ["1,469,199 km (912,918 mi)","1,104,306 km (686,184 mi)","1,392,684 km (865,374 mi)","960,000 km (596,516 mi)"],
-  correctAnswer: 2
-},
-{
-  questionId: 3,
-  question:"How many moons does Jupiter have?",
-  answers: ["24","66","94","112"],
-  correctAnswer: 1
-}
+var questions = [
+  {
+    questionId: 1,
+    question: "How many planets are in the solar system?",
+    answers: [9,8,10,11],
+    correctAnswer: 8
+  },
+  {
+    questionId: 2,
+    question: "What is the diameter of the Sun?",
+    answers: ["1,469,199 km (912,918 mi)","1,104,306 km (686,184 mi)","1,392,684 km (865,374 mi)","960,000 km (596,516 mi)"],
+    correctAnswer: "1,104,306 km (686,184 mi)"
+  },
+  {
+    questionId: 3,
+    question:"How many moons does Jupiter have?",
+    answers: [24,66,94,112],
+    correctAnswer: 66
+  }
 ];
 
-console.log(myQuestions[0].correctAnswer);
-// Need to recognize the click event in irder to begin the trivia Game
+//On click of the star button, begin the Game
 
-//Creating a function to display a question
-for (var i = 0; i < myQuestions.length; i++) {
-  var selectedQuestion = 1;
-    if (selectedQuestion == myQuestions[i].questionId){
-      $(actualQuestion).append("<h2>" + myQuestions[i].question + "</h2>");
+$("#start").click(function(){
+  $("#start").remove();
+  nextQuestion();
+});
 
-      for (var j = 0; j < myQuestions[i].answers.length; j++) {
-        // templateLiteral = `string${mylist}`
-        $(answerChoices).append("<button class = answerButton + id= " + myQuestions[i].answers[j] + " value = " + myQuestions[i].answers[j] + " > " + myQuestions[i].answers[j] + "</button><br>");
+//Creeating the function for the timer to start at 30 and work its way down to 0
+
+function countDowntimer() {
+  var timeleft = 30;
+  $("#timer").show();
+  $("#timer").text(timeleft);
+  downloadTimer = setInterval(function(){
+    timeleft--;
+    $("#timer").text(timeleft);
+    if (timeleft <= 0) {
+      unanswered++;
+      questionIncrement();
+      if (questionNumber > questions.length - 1) {
+        clearInterval(downloadTimer);
+        results();
+      } else {
+        nextQuestion();
       }
     }
+  }, 1000);
+}
+
+
+//Increment through each question
+function questionIncrement(){
+  questionNumber++;
+}
+
+console.log('questionNumber: ' + questionNumber);
+console.log('number of questions ' + questions.length);
+
+//Increment through each answer answerChoice
+function answerIncrement() {
+  $("#answerChoice").text("");
+  for (var i = 0; i < questions[questionNumber].answers.length; i++) {
+    $("#answerChoice").append("<button class = answerButton + id= " + questions[questionNumber].answers[i] + " value = " + questions[questionNumber].answers[i] + " > " + questions[questionNumber].answers[i] + "</button><br>");
+  }
+  $("#answerChoice").show();
+}
+
+//Creating a function to display the results
+function results(){
+  // Hide all other divs on screen
+  $("#timer").hide();
+  $("#question").hide();
+  $("#answerChoice").hide();
+  $("#correctAnswer").hide();
+  $("#nextButton").hide();
+  //Add text to show the user they are done
+  //Add the amount correct
+  //Add the amount incorrect
+  //Add the amount unanswered
+  //Add a button to start over
+  $("#results").append('<h2 class="numberCorrect">Correct Answers: <span> '+ correct + '</span></h2>');
+  $("#results").append('<h2 class="numberIncorrect">Incorrect Answers: <span> '+ incorrect + '</span></h2>');
+  $("#results").append('<h2 class="numberUnanswered">Incorrect Answers: <span> '+ unanswered + '</span></h2>');
+}
+
+function nextQuestion() {
+  clearInterval(downloadTimer);
+
+  if (questionNumber > questions.length - 1) {
+    results();
+    return;
   }
 
-//click values
+  //Advance to the next Question
 
+  console.log('questionNumber: ' + questionNumber);
+  //begin countdown timer
+  countDowntimer();
 
+  console.log('Question ' + questionNumber);
+  //Add the question text
+  $("#question").text(questions[questionNumber].question);
+  $("#correctAnswer").text("");
 
+  //Add the potential answers
+  answerIncrement();
 
-//Display the next question in the Game
-//Display all the potential answers
-  //Recognize the answer selected
-    //If correct, display that the answer was correct
-    //If incorrect, display that the answer was incorrect
-//Loop through each question until all the questions have been answered
-//Display the results at the end
+  $(".answerButton").click(function(){
+    clearInterval(downloadTimer);
+    console.log($(this).text());
+    if ($(this).text().trim() == questions[questionNumber].correctAnswer){
+      $("#timer").replaceWith('<h2 id="timer">Correct Answer!</h2>');
+      correct++;
+      console.log('correct ' + correct);
+    } else {
+      $("#timer").replaceWith('<h2 id="timer">Wrong Answer! Better Luck Next TIme</h2>');
+      incorrect++;
+    }
+
+    $("#answerChoice").hide();
+    $("#correctAnswer").text(questions[questionNumber].correctAnswer);
+    $("#next").append("<button class = answerButton + id='nextButton'>Next</button>");
+    $("#nextButton").click(function() {
+      questionIncrement();
+      nextQuestion();
+      $("#next").text("");
+    });
+
+  });
+
+}
